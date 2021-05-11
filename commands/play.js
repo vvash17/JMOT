57b08e7ff9ae7e7561bd25a0f4664b61418c30bc
vvash17 <vvash17@freeuni.edu.ge>
2021-05-11 18:45:19 +0400

    added play functional to bot, added dynamic approach of command load

---
+++ commands/play.js
@@ -0,0 +1,39 @@
+const ytdl = require('ytdl-core');
+const ytsearch = require('yt-search');
+
+module.exports = {
+    name : 'play',
+    description : 'Joins and plays a video from youtube',
+    async execute(message, args) {
+        const voiceChannel = message.member.voice.channel;
+
+        if (!voiceChannel) return message.channel.send('You need to be in channel to play a song');
+        const permissions = voiceChannel.permissionsFor(message.client.user);
+        if (!permissions.has('CONNECT')) return message.channel.send('You do not have permission to play a song');
+        if (!permissions.has('SPEAK')) return message.channel.send('You do not have permission to speak');
+        if (!args.length) return message.channel.send('You need to specify song name');
+
+        const connection = await voiceChannel.join();
+
+        const videoFinder = async (query) => {
+
+            const videoResult = await ytsearch(query);
+
+            return (videoResult.videos.length > 1) ? videoResult.videos[0] : null;
+        }
+
+        const video = await videoFinder(args.join(' '));
+
+        if(video){
+            const stream = ytdl(video.url, {filter: 'audioonly'});
+            connection.play(stream, {seek: 0, volume: 1})
+            .on('finish', () => {
+                voiceChannel.leave();
+            })
+
+            await message.reply(':thumbsup: NowPlaying ***'.concat(video.title).concat('***'));
+        } else {
+            message.channel.send('Song not found');
+        }
+    }
+}
--- main.js
+++ main.js
@@ -9,11 +9,11 @@ jmotClient.commands = new Discord.Collection();
 
 const commandFiles = FS.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for(const file of commandFiles){
-    console.log(file);
     const command = require('./commands/'.concat(file));
     jmotClient.commands.set(command.name, command);
 }
-jmotClient.login('ODQxNjYyNDcyNTU2NTExMjQy.YJqBQg.JRZ7ZN3MroWNXvE-pUQm3Q3OYlY');
+
+jmotClient.login(require('./config.json').jmotToken);
 
 jmotClient.once('ready', () => {
     console.log('JMOT is online');
@@ -25,7 +25,8 @@ jmotClient.on('message', message => {
    const arguments = message.content.slice(prefix.length).split(/ +/);
    const command = arguments.shift().toLowerCase();
 
-   if(command === 'greeting') {
+   if(command) {
        jmotClient.commands.get(command).execute(message, arguments);
    }
+
 });
